37d5277bd7fecc3b2b435c58e0304dee14df7709
Jocelyn Wu <jocelyn.wu@berkeley.edu>
2026-01-18 13:02:28 -0800

    Initial project setup and GET actions endpoint

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # SustainabilityTracker
 Built a Django REST API to manage and track sustainability actions. Additionally, created a basic frontend using React to interact with the API. 
+
+
+
+client 
+need vite installed
+SustainabilityTracker\client\app> yarn dev (run)
+Local:   http://localhost:5173/
+
+
+
+IN sustainbilitytracker/server
+need python installed -- confirm you have it python --version and something should pop up
+need pip installed 
+then download django and djangoframework in terminal 
+pip install django djangorestframework
+
+to run... SustainabilityTracker\server\newproject> python manage.py runserver
+Starting development server at http://127.0.0.1:8000/
+
+
+https://docs.djangoproject.com/en/6.0/ref/models/fields/
+
+
+
+how frontend communicates with backend 
+django-cors-headers
+
+inside server --> pip install django-cors-headers
---
+++ server/newproject/api/views.py
@@ -0,0 +1,23 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Action
+from .serializer import ActionSerializer
+
+@api_view(["GET"])
+def get_actions(request):
+    actions = Action.objects.all() #variable actions - Python Model
+    actions_serialized = ActionSerializer(actions, many = True).data #now a JSON
+    return Response(actions_serialized)
+
+
+@api_view(["POST"])
+def create_action(request):  #request --> body of data (JSON)
+    data = request.data #JSON format --> python dictionary
+    data_serialized = ActionSerializer(data=data) #converted into serializer object
+    if data_serialized.is_valid(): #whenever frontend sends you data, you have to make sure valid data
+        data_serialized.save() #creates a Django model instance
+        return Response(data_serialized.data, status = status.HTTP_201_CREATED) #so basically returns a python dict (but frontend sees it as JSON)
+    return Response(data_serialized.errors, status = status.HTTP_400_BAD_REQUEST)
+  
