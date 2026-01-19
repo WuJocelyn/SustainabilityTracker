94dd8958661de6c0b1d0de09acac3f1e7a8fd5aa
Jocelyn Wu <jocelyn.wu@berkeley.edu>
2026-01-19 01:12:24 -0800

    Added Comments and Formatting Adjustments

--- server/newproject/api/models.py
+++ server/newproject/api/models.py
@@ -1,5 +1,16 @@
 from django.db import models
 
+"""
+Action Model
+------------
+Represents one sustainability action entry.
+
+Fields:
+- action (str): Short description of the activity (e.g., "Recycling")
+- date (date): The date the action was performed
+- points (int): Number of sustainability points awarded for the action
+"""
+
 class Action(models.Model):
     action = models.CharField(max_length= 50)
     date = models.DateField()
